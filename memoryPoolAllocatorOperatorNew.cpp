b74bdf3ed5fea14eee3e76ebb74ee6aad0bb494c
Kaleem Ahmad Khan <014.kmkhan@gmail.com>
2026-01-02 20:34:06 +0530

    Add MemoryPool class for dynamic memory allocation
    Implement a memory pool allocator using the new operator, allowing for dynamic memory management and object construction in place.

--- memoryPoolAllocatorOperatorNew.cpp
+++ memoryPoolAllocatorOperatorNew.cpp
@@ -1,1 +1,117 @@
+/*
+  Implement Pool using new operator
+*/
+template<typename T>
+class MemoryPool {
+  std::vector<T*> m_pool; // Stores available memory for allocation
+  std::vector<void*> m_toFree; // Stores allocations to delete on Dtor
+  size_t m_reallocSize{0}; // Stores reallocation size if pool runs out of mememory
+  size_t m_numAllocated{0}; // Store the number of T for which memory is allocated
 
+public:
+
+  explicit MemoryPool(size_t nAllocations, size_t nReAllocSize = 1<<8) {
+    m_reallocSize = std::max<size_t>(nReAllocSize, 1);
+    if (nAllocations == 0) {
+      return;
+    }
+    void* rawBytes = allocateRaw(nAllocations);
+    addToPool(rawBytes, nAllocations, sizeof(T));
+    m_numAllocated += nAllocations;
+  }
+  
+  //Rule of 5 : Disable Copy / Move
+  ~MemoryPool() {
+    // When Dtor calls user needs to ensure that dealloc is called on each alloc, otherwise there will be a memory leak
+    for (void* toFree : m_toFree) {
+      ::operator delete[](toFree, std::align_val_t(alignof(T)));
+    }
+  }
+  MemoryPool(const MemoryPool&) = delete;
+  MemoryPool& operator=(const MemoryPool&) = delete;
+  // Rule of 5 end
+
+  void* allocateRaw(size_t nAllocations) {
+    // Get Raw Bytes with Alignment
+    return ::operator new[](nAllocations * sizeof(T), std::align_val_t(alignof(T)));
+  }
+
+  // Adds memory available to pool
+  void addToPool(void* rawBytesAllocatedPtr, size_t nAllocations, size_t allocationSize) {
+    T* startAddress = reinterpret_cast<T*>(rawBytesAllocatedPtr);
+    for(size_t i = 0; i < nAllocations; i++) {
+      m_pool.push_back(startAddress + i);
+    }
+    m_toFree.push_back(rawBytesAllocatedPtr);
+    m_numAllocated += nAllocations;
+  }
+
+  // Resizes pool
+  void resize(size_t nAllocations) {
+    void* rawBytes = allocateRaw(nAllocations);
+    addToPool(rawBytes, nAllocations, sizeof(T));
+    m_numAllocated += nAllocations;
+  }
+
+  // Returns address to construct object
+  T* alloc() {
+    // Resize
+    if (m_pool.empty()) {
+      resize(m_reallocSize);
+    }
+    // Get Allocation
+    T* allocatePtr = m_pool.back();
+    // Remove from pool
+    m_pool.pop_back(); 
+    return allocatePtr;
+  }
+
+  // Dealocates object and frees up memory in pool : Call this in Dtor of T
+  void dealloc(T* ptr) {
+    ptr->~T();
+    m_pool.push_back(ptr);
+  }
+
+  // Function to construct T in place
+  template<typename... ArgsT>
+  T* make(ArgsT&&... args) {
+    return new (alloc()) T(std::forward<ArgsT>(args)...);
+  }
+
+  // Getters
+  size_t allocated() const {
+    return m_numAllocated;
+  }
+
+  size_t available() const {
+    return m_pool.size();
+  }
+
+  // Setters
+  size_t adjustReallocSize(size_t size) {
+    assert(size != 0);
+    m_reallocSize = size;
+  }
+};
+
+int main() {
+  MemoryPool<int> pool{2};
+  int* p1 = pool.make(125);
+  int* p2 = new (pool.alloc()) int(54);
+  assert(*p1 == 125);
+  assert(*p2 == 54);
+  pool.dealloc(p1);
+  int* p3 = pool.make(456);
+  assert(*p3 == 456);
+  pool.dealloc(p2);
+  pool.dealloc(p3);
+
+  struct A {
+    A() { std::cout << "Constucting\n"; }
+    ~A() { std::cout << "Destructing\n"; }
+  };
+  MemoryPool<A> pool1{1};
+  A* p4 = pool1.make();
+  pool1.dealloc(p4);
+
+}
